bb916da011337abdb35916c9a42a27b24c286754
Angelika Hagen-Breaux <hagenafh@dfw.wa.gov>
2019-12-12 12:24:21 -0800

    adding active rate scaler and exes to bin/debug

---
+++ FVS_ActiveRateScaler.Designer.vb
@@ -0,0 +1,92 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FVS_ActiveRateScaler
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.StkFishRateScalerGrid = New System.Windows.Forms.DataGridView
+        Me.lblSFRSTitle = New System.Windows.Forms.Label
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.TextBox1 = New System.Windows.Forms.TextBox
+        CType(Me.StkFishRateScalerGrid, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'StkFishRateScalerGrid
+        '
+        Me.StkFishRateScalerGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.StkFishRateScalerGrid.Location = New System.Drawing.Point(50, 89)
+        Me.StkFishRateScalerGrid.Name = "StkFishRateScalerGrid"
+        Me.StkFishRateScalerGrid.Size = New System.Drawing.Size(980, 507)
+        Me.StkFishRateScalerGrid.TabIndex = 0
+        '
+        'lblSFRSTitle
+        '
+        Me.lblSFRSTitle.AutoSize = True
+        Me.lblSFRSTitle.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSFRSTitle.Location = New System.Drawing.Point(46, 31)
+        Me.lblSFRSTitle.Name = "lblSFRSTitle"
+        Me.lblSFRSTitle.Size = New System.Drawing.Size(322, 24)
+        Me.lblSFRSTitle.TabIndex = 1
+        Me.lblSFRSTitle.Text = "Active Stock Fishery Rate Scalers"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(466, 621)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(151, 35)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "OK - Done"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.Control
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.ForeColor = System.Drawing.Color.Red
+        Me.TextBox1.Location = New System.Drawing.Point(50, 63)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(481, 15)
+        Me.TextBox1.TabIndex = 4
+        Me.TextBox1.Text = "Use previous screen for updating scalers, if desired!!!!"
+        '
+        'FVS_ActiveRateScaler
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1122, 686)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.lblSFRSTitle)
+        Me.Controls.Add(Me.StkFishRateScalerGrid)
+        Me.Name = "FVS_ActiveRateScaler"
+        Me.Text = "FVS_ActiveRateScaler"
+        CType(Me.StkFishRateScalerGrid, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents StkFishRateScalerGrid As System.Windows.Forms.DataGridView
+    Friend WithEvents lblSFRSTitle As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+End Class
---
+++ FVS_ActiveRateScaler.vb
@@ -0,0 +1,166 @@
+﻿Public Class FVS_ActiveRateScaler
+    Public GridLoading As Boolean
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Me.Visible = False
+        FVS_InputMenu.Visible = True
+        FVS_InputMenu.Refresh()
+    End Sub
+
+    Private Sub FVS_ActiveRateScaler_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        FormHeight = 847
+        FormWidth = 1049
+        Dim fish As Integer
+
+        ' - Check if Form fits within Screen Dimensions
+        If (FormHeight > My.Computer.Screen.Bounds.Height Or _
+            FormWidth > My.Computer.Screen.Bounds.Width) Then
+            Me.Height = FormHeight / (DevHeight / My.Computer.Screen.Bounds.Height)
+            Me.Width = FormWidth / (DevWidth / My.Computer.Screen.Bounds.Width)
+            If FVS_ActiveRateScaler_ReSize = False Then
+                Resize_Form(Me)
+                FVS_ActiveRateScaler_ReSize = True
+            End If
+        End If
+
+        GridLoading = True
+        StkFishRateScalerGrid.Columns.Clear()
+        StkFishRateScalerGrid.ColumnHeadersDefaultCellStyle.Font = New Font("Microsoft San Serif", CInt(10 / FormWidthScaler), FontStyle.Bold)
+        StkFishRateScalerGrid.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.BottomCenter
+        StkFishRateScalerGrid.DefaultCellStyle.Font = New Font("Microsoft San Serif", CInt(10 / FormWidthScaler), FontStyle.Bold)
+
+        StkFishRateScalerGrid.Columns.Add("Fishery", "Fishery")
+        StkFishRateScalerGrid.Columns("Fishery").Width = 100 / FormWidthScaler
+        StkFishRateScalerGrid.Columns("Fishery").ReadOnly = True
+        StkFishRateScalerGrid.Columns("Fishery").DefaultCellStyle.BackColor = Color.Aquamarine
+
+        StkFishRateScalerGrid.Columns.Add("FishNum", "Fish#")
+        StkFishRateScalerGrid.Columns("FishNum").Width = 50 / FormWidthScaler
+        StkFishRateScalerGrid.Columns("FishNum").ReadOnly = True
+        StkFishRateScalerGrid.Columns("FishNum").DefaultCellStyle.Alignment = DataGridViewContentAlignment.BottomCenter
+        StkFishRateScalerGrid.Columns("FishNum").DefaultCellStyle.BackColor = Color.Azure
+
+        StkFishRateScalerGrid.Columns.Add("Stock", "Stock")
+        StkFishRateScalerGrid.Columns("Stock").Width = 100 / FormWidthScaler
+        StkFishRateScalerGrid.Columns("Stock").ReadOnly = True
+        StkFishRateScalerGrid.Columns("Stock").DefaultCellStyle.Alignment = DataGridViewContentAlignment.BottomCenter
+        StkFishRateScalerGrid.Columns("Stock").DefaultCellStyle.BackColor = Color.Azure
+
+        StkFishRateScalerGrid.Columns.Add("StkNum", "Stk#")
+        StkFishRateScalerGrid.Columns("StkNum").Width = 50 / FormWidthScaler
+        StkFishRateScalerGrid.Columns("StkNum").ReadOnly = True
+        StkFishRateScalerGrid.Columns("StkNum").DefaultCellStyle.Alignment = DataGridViewContentAlignment.BottomCenter
+        StkFishRateScalerGrid.Columns("StkNum").DefaultCellStyle.BackColor = Color.Azure
+
+
+        StkFishRateScalerGrid.Columns.Add("Time1", "Time1")
+        StkFishRateScalerGrid.Columns("Time1").Width = 100 / FormWidthScaler
+        StkFishRateScalerGrid.Columns("Time1").DefaultCellStyle.Format = ("###0.0000")
+        StkFishRateScalerGrid.Columns("Time1").DefaultCellStyle.Alignment = DataGridViewContentAlignment.BottomCenter
+
+        StkFishRateScalerGrid.Columns.Add("Time2", "Time2")
+        StkFishRateScalerGrid.Columns("Time2").Width = 100 / FormWidthScaler
+        StkFishRateScalerGrid.Columns("Time2").DefaultCellStyle.Format = ("###0.0000")
+        StkFishRateScalerGrid.Columns("Time2").DefaultCellStyle.Alignment = DataGridViewContentAlignment.BottomCenter
+
+        StkFishRateScalerGrid.Columns.Add("Time3", "Time3")
+        StkFishRateScalerGrid.Columns("Time3").Width = 100 / FormWidthScaler
+        StkFishRateScalerGrid.Columns("Time3").DefaultCellStyle.Format = ("###0.0000")
+        StkFishRateScalerGrid.Columns("Time3").DefaultCellStyle.Alignment = DataGridViewContentAlignment.BottomCenter
+
+        StkFishRateScalerGrid.Columns.Add("Time4", "Time4")
+        StkFishRateScalerGrid.Columns("Time4").Width = 100 / FormWidthScaler
+        StkFishRateScalerGrid.Columns("Time4").DefaultCellStyle.Format = ("###0.0000")
+        StkFishRateScalerGrid.Columns("Time4").DefaultCellStyle.Alignment = DataGridViewContentAlignment.BottomCenter
+
+        If SpeciesName = "COHO" Then
+            StkFishRateScalerGrid.Columns.Add("Time5", "Time5")
+            StkFishRateScalerGrid.Columns("Time5").Width = 100 / FormWidthScaler
+            StkFishRateScalerGrid.Columns("Time5").DefaultCellStyle.Format = ("###0.0000")
+            StkFishRateScalerGrid.Columns("Time5").DefaultCellStyle.Alignment = DataGridViewContentAlignment.BottomCenter
+        End If
+
+
+
+        Dim rowcount As Integer = 0
+        If SpeciesName = "COHO" Then
+            For fish = 1 To NumFish
+                For Stk As Integer = 1 To NumStk
+                    If StockFishRateScalers(Stk, fish, 1) <> 1.0 Or StockFishRateScalers(Stk, fish, 2) <> 1.0 Or StockFishRateScalers(Stk, fish, 3) <> 1.0 _
+                    Or StockFishRateScalers(Stk, fish, 4) <> 1.0 Or StockFishRateScalers(Stk, fish, 5) <> 1 Then
+                        rowcount = rowcount + 1
+                    End If
+                Next
+            Next
+
+        Else
+            For fish = 1 To NumFish
+                For Stk As Integer = 1 To NumStk
+                    If StockFishRateScalers(Stk, fish, 1) <> 1.0 Or StockFishRateScalers(Stk, fish, 2) <> 1.0 Or StockFishRateScalers(Stk, fish, 3) <> 1.0 Or StockFishRateScalers(Stk, fish, 4) <> 1.0 Then
+                        rowcount = rowcount + 1
+                    End If
+                Next
+            Next
+        End If
+
+        StkFishRateScalerGrid.RowCount = rowcount + 1
+
+        rowcount = 0
+        If SpeciesName = "COHO" Then
+            For fish = 1 To NumFish
+                For Stk As Integer = 1 To NumStk
+                    If StockFishRateScalers(Stk, fish, 1) <> 1.0 Or StockFishRateScalers(Stk, fish, 2) <> 1.0 Or StockFishRateScalers(Stk, fish, 3) <> 1.0 _
+                    Or StockFishRateScalers(Stk, fish, 4) <> 1.0 Or StockFishRateScalers(Stk, fish, 5) <> 1 Then
+                        For TStep = 1 To NumSteps
+                            If AnyBaseRate(fish, TStep) = 0 Then
+                                StkFishRateScalerGrid.Item(0, rowcount).Value = FisheryName(fish)
+                                StkFishRateScalerGrid.Item(1, rowcount).Value = FisheryID(fish)
+                                StkFishRateScalerGrid.Item(2, rowcount).Value = StockName(Stk)
+                                StkFishRateScalerGrid.Item(3, rowcount).Value = StockID(Stk)
+                                StkFishRateScalerGrid.Item(TStep + 3, rowcount).Value = "****"
+                            Else
+                                StkFishRateScalerGrid.Item(0, rowcount).Value = FisheryName(fish)
+                                StkFishRateScalerGrid.Item(1, rowcount).Value = FisheryID(fish)
+                                StkFishRateScalerGrid.Item(2, rowcount).Value = StockName(Stk)
+                                StkFishRateScalerGrid.Item(3, rowcount).Value = StockID(Stk)
+                                StkFishRateScalerGrid.Item(TStep + 3, rowcount).Value = StockFishRateScalers(Stk, fish, TStep).ToString("###0.0000")
+                            End If
+
+                        Next TStep
+                        rowcount = rowcount + 1
+                    End If
+                Next
+            Next
+
+        Else
+            For fish = 1 To NumFish
+                For Stk As Integer = 1 To NumStk
+                    If StockFishRateScalers(Stk, fish, 1) <> 1.0 Or StockFishRateScalers(Stk, fish, 2) <> 1.0 Or StockFishRateScalers(Stk, fish, 3) <> 1.0 Or StockFishRateScalers(Stk, fish, 4) <> 1.0 Then
+                        For TStep = 1 To NumSteps
+                            If AnyBaseRate(fish, TStep) = 0 Then
+                                StkFishRateScalerGrid.Item(0, rowcount).Value = FisheryName(fish)
+                                StkFishRateScalerGrid.Item(1, rowcount).Value = FisheryID(fish)
+                                StkFishRateScalerGrid.Item(2, rowcount).Value = StockName(Stk)
+                                StkFishRateScalerGrid.Item(3, rowcount).Value = StockID(Stk)
+                                StkFishRateScalerGrid.Item(TStep + 3, rowcount).Value = "****"
+                               
+                            Else
+                                StkFishRateScalerGrid.Item(0, rowcount).Value = FisheryName(fish)
+                                StkFishRateScalerGrid.Item(1, rowcount).Value = FisheryID(fish)
+                                StkFishRateScalerGrid.Item(2, rowcount).Value = StockName(Stk)
+                                StkFishRateScalerGrid.Item(3, rowcount).Value = StockID(Stk)
+                                StkFishRateScalerGrid.Item(TStep + 3, rowcount).Value = StockFishRateScalers(Stk, fish, TStep).ToString("###0.0000")
+                            End If
+
+                        Next TStep
+                        rowcount = rowcount + 1
+
+                    End If
+                Next
+            Next
+        End If
+
+        GridLoading = False
+    End Sub
+End Class
